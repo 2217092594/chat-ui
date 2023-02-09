4f9790599a769fe5dae2024a837e676b5f4a838d
zhangxiaolong <dragonmaxzxl@163.com>
2023-02-09 09:52:21 +0800

    即将完成

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -103,7 +103,7 @@ module.exports = function (proxy, allowedHost) {
     // `proxy` is run between `before` and `after` `webpack-dev-server` hooks
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // 后台服务地址以及端口号
+        target: 'http://localhost:8899', // 后台服务地址以及端口号
         ws: true,
         changeOrigin: true, //是否跨域
         pathRewrite: { '^/api': '/' }
